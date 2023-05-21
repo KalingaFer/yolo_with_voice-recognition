79179c6c5def24b227a43c3bd93b26af51802630
KalingaFer <kaalingaa@gmail.com>
2023-05-21 12:53:59 +0530

    'modified'

--- project/yolonew/detect.py
+++ project/yolonew/detect.py
@@ -126,10 +126,12 @@ def run(
         with dt[1]:
             visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
             pred = model(im, augment=augment, visualize=visualize)
+	
 
         # NMS
         with dt[2]:
             pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+            
 
         # Second-stage classifier (optional)
         # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
